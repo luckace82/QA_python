edb3ee9481dfcc764f01b86512948282bb18f24a
Your Name <lakeshshrestha82@gmail.com>
2023-07-14 16:38:24 +0545

    bootstrap

--- question_answer/q_a/models.py
+++ question_answer/q_a/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Question(models.Model):
-    question_txt=models.CharField(max_length=100)
+    question_txt=models.CharField(max_length=1000)
     pub_date=models.DateTimeField("published_date")
     exp_sol=models.TextField(max_length=1000,default="____")
     answer=models.TextField(max_length=1000,default="____")
