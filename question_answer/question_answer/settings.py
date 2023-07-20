982148cacf4e261fde9971b424d02ec1ca75be17
Your Name <lakeshshrestha82@gmail.com>
2023-07-20 10:37:09 +0545

    some changes instatic files

--- question_answer/question_answer/settings.py
+++ question_answer/question_answer/settings.py
@@ -122,11 +122,9 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
 
-#Add this in your settings.py file:
+
 STATICFILES_DIRS = [
     BASE_DIR / 'mystaticfiles'
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
